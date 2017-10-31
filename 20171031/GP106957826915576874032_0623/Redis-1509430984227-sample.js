f9c9849db6ab6e6c042d3fa9cf519a0fe4d736f0
snippet-java <snippet.java@gmail.com>
2017-10-31 14:23:04 +0800

    Commit from hacker-place

---
+++ 20171031/GP106957826915576874032_0623/Redis-1509430984227-sample.js
@@ -0,0 +1,52 @@
+var testparams = {
+    "hostname": "", 
+    "password": "",
+    "port": "",
+    "key": "hashkey1",
+    "field": "hashfield1"
+};
+var redis = require('redis');
+
+function main(params) {
+    var redisconn = {
+        hostname : params.hostname || "SANDBOX_REDIS_HOST", 
+        password : params.password || "SANDBOX_REDIS_PASSWORD",
+        port : params.port || "SANDBOX_REDIS_PORT"
+    };
+
+    var client = redis.createClient({host: redisconn.hostname, password: redisconn.password, port:redisconn.port});
+
+    return new Promise(function(resolve, reject) {
+        if(params.field) {
+            client.hget(params.key, params.field, function(err, data) {
+                client.end(true);
+                if (err) {
+                    return reject({err});
+                }
+        
+                return resolve({data});
+            });
+        }
+        else {
+            client.hgetall(params.key, function(err, data) {
+                client.end(true);
+                if (err) {
+                    return reject({err});
+                }
+        
+                return resolve({data});
+            });
+        }
+    })
+}
+
+if (require.main === module) {
+    main(testparams).then(function(result) {
+        console.log(JSON.stringify(result));
+    })
+    .catch(function (err) {
+        console.error(err);
+    });
+}
+
+exports.main = main;
