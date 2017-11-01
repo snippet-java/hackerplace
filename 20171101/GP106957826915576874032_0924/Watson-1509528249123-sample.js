f521ee79784eb9c2079bfc6fff792a6edd26a072
snippet-java <snippet.java@gmail.com>
2017-11-01 17:24:09 +0800

    Commit from hacker-place

---
+++ 20171101/GP106957826915576874032_0924/Watson-1509528249123-sample.js
@@ -0,0 +1,50 @@
+const testparams = {
+  "nlu_source": "http://www.techradar.com/news/phone-and-communications/mobile-phones/20-best-mobile-phones-in-the-world-today-645440",
+  "username": "",
+  "password": "",
+  "url": "https://sandbox-watson-proxy.mybluemix.net/natural-language-understanding/api",
+  "use_unauthenticated": true
+}
+
+function main(params) {
+  return new Promise(function (resolve, reject) {
+    const NaturalLanguageUnderstandingV1 = require("watson-developer-cloud/natural-language-understanding/v1.js");
+
+    var natural_language_understanding = new NaturalLanguageUnderstandingV1({
+      "username": params.username || "",
+      "password": params.password || "",
+      "version_date" : NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27,
+      "url": params.url || "https://gateway.watsonplatform.net/natural-language-understanding/api",
+      "use_unauthenticated": isTrue(params.use_unauthenticated)
+    });
+
+    var parameters = {
+        url : params.nlu_source, 
+        features : {
+            keywords : {
+                'limit' : 250
+            } 
+        }
+    };
+
+    natural_language_understanding.analyze(parameters, function (err, res) {
+      if (err)
+        reject(err);
+      else
+        resolve(res);
+    });
+  });
+}
+
+function isTrue(value) {
+  if (value == "true" || value == true)
+    return true;
+  return false;
+}
+
+if (require.main === module)
+  main(testparams)
+    .then((results) => console.log(JSON.stringify(results, null, 2)))
+    .catch((error) => console.log(error.message));
+
+exports.main = main;
